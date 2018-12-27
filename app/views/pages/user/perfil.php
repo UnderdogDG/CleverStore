33ff212f4c699ac8600f2e9c91788e9309a4fc2b
Underdog - laptop <sugarskullgb@gmail.com>
2018-12-27 16:16:32 -0600

    +CSS +Perfil

--- app/views/pages/user/perfil.php
+++ app/views/pages/user/perfil.php
@@ -3,16 +3,25 @@
 ?>
 
 <div class="section">
-  <div class="container_register">
-    <div class="field">
-      <h2>Perfil</h2>
+  <!-- <div class="container_register"> -->
+    <div class="form_black">
+      <div class="field">
+        <h1>Perfil</h1>
+      </div>
+
+      <div class="field">
+        <div class="photo">
+          <img src="<?= URL . '/public/img/upload/' . $_SESSION["img"]?>" alt="<?= $_SESSION["name"]?>">
+        </div>
+      </div>
+      
+      <div class="field">
+        <h2><?= $_SESSION["name"]?></h2>
+      </div>
+        <a href="<?= URL ?>/user/logout" class="btn btn_red md"><span>Salir</span></a>
+      <?= print_r($_SESSION);?>
     </div>
-    <div class="img">
-      <img src="<?= URL . '/public/img/upload/' . $_SESSION["img"]?>" alt="">
-    </div>
-    <?= print_r($_SESSION);?>
-    <a href="<?= URL ?>/user/logout" class="btn-green">Logout</a>
-  </div>
+  <!-- </div> -->
 </div>
 
 <?php
