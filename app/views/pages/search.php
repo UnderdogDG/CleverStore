b8cfa035328aabe2e2d51fe71793307d2b2d3011
Underdog-Main <sugarskullgb@gmail.com>
2019-01-16 10:16:10 -0600

    ++no found

--- app/views/pages/search.php
+++ app/views/pages/search.php
@@ -10,30 +10,42 @@
     <div class="section section_nofull">
       <div class="container_search">
 
-        <?php foreach ($data as $item) : ?>
-
-        <div class="item wow fadeInUp" data-wow-delay="<?= $i += 0.1; ?>">
-          <div class="container">
-            <div class="container_head">
-              <h2><span><?= $item["name"]; ?></span></h2>
-              <a href="<?= URL . "/product/item/" . $item["sku"]?>" class="btn btn_gold md"><span>Ir al Producto</span></a>
-            </div>
-
-            <div class="container_img">
-              <img src="<?=URL ?>/img/store/anillo.png" alt="<?= $item["name"]; ?>">
-            </div>
-
-            <div class="container_options">
-              <button class="btn_pop"><i class="far fa-star fav"></i></button>
-              <button class="btn_pop"><i class="fas fa-gift"></i></button>
+        <?php if(!$data) : ?>
+          <div class="item wow fadeInUp">
+            <div class="container">
+              <div class="noFound">
+                <h2>Lo sentimos</h2>
+                <h3>No se encontraron ítems con esta búsqueda</h2>
+              </div>
             </div>
           </div>
-        </div>
 
+        <?php else : ?>
 
+          <?php foreach ($data as $item) : ?>
 
-        <?php endforeach;  ?>
-        <!-- <?= print_r($data); ?> -->
+          <div class="item wow fadeInUp" data-wow-delay="<?= $i += 0.1; ?>">
+            <div class="container">
+              <div class="container_head">
+                <h2><span><?= $item["name"]; ?></span></h2>
+                <a href="<?= URL . "/product/item/" . $item["sku"]?>" class="btn btn_gold md"><span>Ir al Producto</span></a>
+              </div>
+
+              <div class="container_img">
+                <img src="<?=URL ?>/img/store/anillo.png" alt="<?= $item["name"]; ?>">
+              </div>
+
+              <div class="container_options">
+                <button class="btn_pop"><i class="far fa-star fav"></i></button>
+                <button class="btn_pop"><i class="fas fa-gift"></i></button>
+              </div>
+            </div>
+          </div>
+
+          <?php endforeach;  ?>
+          <!-- <?= print_r($data); ?> -->
+
+        <?php endif; ?>
       </div>
     </div>
   </main>
